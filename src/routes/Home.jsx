de31a65163c5aa63403e4ceb55e2f6da4dc02c4c
Jannis Jorre <jannis@jorre.dev>
2019-05-29 13:33:25 +0200

    (feat): created basic landing page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,10 +2,14 @@ import { h } from 'preact';
 import style from './home.css';
 
 const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
+	<main class={style.home}>
+		<img class={style.image} src="../assets/jeyj0.png" />
+		<h1>Jannis Jorre</h1>
+		<h2>Software Developer</h2>
+		<h3>Focused on Web (Frontend)</h3>
+		<p>Studying at <a href="https://code.berlin/en/" target="_blank">CODE University of Applied Sciences</a></p>
+		<p>Employed at <a href="https://www.arcusx.com/" target="_blank">arcus[x]</a></p>
+	</main>
 );
 
 export default Home;
