496688d95f5a1dfb6e20b0da9ff9f674d818053c
Jannis Jorre <jannis@jorre.dev>
2020-05-05 19:24:46 +0200

    fix typo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ module.exports = {
     <input type="checkbox" aria-hidden="true" id="nav-toggle" />
     <nav aria-hidden="false">
       <a href="/" aria-label="Go to home page">Home</a>
-      <a href="/about" aria-labl="Go to about page">About</a>
+      <a href="/about" aria-label="Go to about page">About</a>
     </nav>
     <label id="nav-toggle-label" aria-hidden="true" for="nav-toggle" class="noprint">
       <div class="bar"></div>
