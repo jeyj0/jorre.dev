35ed01e6b0ba37955ee3edf1988dd625b52b4d01
Jannis Jorre <jannis@jorre.dev>
2020-05-05 19:14:34 +0200

    remove unused neutral class from link component

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -3,7 +3,7 @@ const { html } = require('htm/preact')
 module.exports = {
   Menubar: () => html`
 <div class="menubar noprint">
-  <a class="neutral" href="/">
+  <a href="/">
     <img class="circle" alt="Portrait of Jannis Jorre" src="/assets/profile.jpg" />
     <span>Jannis Jorre</span>
   </a>
