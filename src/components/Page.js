2e1f68e5def406b2c1958e13866875a3482d1469
Jannis Jorre <jannis@jorre.dev>
2020-05-05 12:18:34 +0200

    improve keyboard usability - better "active" indication

--- src/components/Page.js
+++ src/components/Page.js
@@ -14,9 +14,12 @@ module.exports = {
     <link rel="stylesheet" type="text/css" href="/styles/main.css" />
   </head>
   <body>
+    <a id="link-to-main-content" href="#main-content">Skip to content</a>
     <${Nav} />
     ${menubar ? html`<${Menubar} />` : null}
-    ${children}
+    <div id="main-content">
+      ${children}
+    </div>
     <footer>
       That's it!
     </footer>
