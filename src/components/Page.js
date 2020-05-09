06c1de2ad799608c4376c62466ceca9b4351809e
Jannis Jorre <jannis@jorre.dev>
2020-05-09 13:11:33 +0200

    fix meta tag content separator (; to ,)

--- src/components/Page.js
+++ src/components/Page.js
@@ -9,7 +9,7 @@ module.exports = {
     <title>${title}</title>
     <meta name="description" content="${description}" />
     <meta charset="utf8"/>
-    <meta name="viewport" content="width=device-width; initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#333" />
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="/styles/main.css" />
