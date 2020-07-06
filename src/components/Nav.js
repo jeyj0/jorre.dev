3f4efcd16763f189d29f0a74cf5968b80d269a42
Jannis Jorre <jannis@jorre.dev>
2020-07-06 15:46:53 +0200

    add a link to my css generator to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,6 +6,7 @@ module.exports = {
     <nav aria-hidden="false">
       <a href="/" aria-label="Go to home page">Home</a>
       <a href="/about" aria-label="Go to about page">About</a>
+      <a href="https://css-gen.jorre.dev/" aria-label="Go to CSS Generators">CSS Generator</a>
     </nav>
     <label id="nav-toggle-label" aria-hidden="true" for="nav-toggle" class="noprint">
       <div class="bar"></div>
