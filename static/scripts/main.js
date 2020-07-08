0436479bdd007f7446607879e3118344c76cda44
Jannis Jorre <jannis@jorre.dev>
2020-07-08 13:24:12 +0200

    save the the chosen theme in localStorage if it was manually chosen

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,6 +1,19 @@
-(function () {
+const STORAGE_KEY_THEME = 'theme'
+
+;(function () {
   document.getElementById('nav-toggle').addEventListener('change', navChange)
   document.getElementById('theme-switch-btn').addEventListener('click', switchTheme)
+
+  try {
+    const savedTheme = window.localStorage.getItem(STORAGE_KEY_THEME)
+    if (savedTheme === 'dark') {
+      document.body.classList.add('theme-dark')
+    } else if (savedTheme === 'light') {
+      document.body.classList.add('theme-light')
+    }
+  } catch (e) {
+    console.debug('Could not get saved theme', e)
+  }
 })()
 
 function navChange(e) {
@@ -22,5 +35,11 @@ function switchTheme() {
   } else {
     document.body.classList.add('theme-dark')
     document.body.classList.remove('theme-light')
+  }
+
+  try {
+    window.localStorage.setItem(STORAGE_KEY_THEME, isLightEnabled ? 'dark' : 'light')
+  } catch (e) {
+    console.debug('Could not save theme preference', e)
   }
 }
